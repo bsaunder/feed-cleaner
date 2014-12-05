21e24d66b6544c747d0ad439f4959f51132864d2
Bryan Saunders <bsaunders@localhost.localdomain>
2014-12-04 23:00:42 -0500

    Converted to edtFTPj/Free from Commons-Net

--- src/main/java/net/bryansaunders/feed_cleaner/FeedCleaner.java
+++ src/main/java/net/bryansaunders/feed_cleaner/FeedCleaner.java
@@ -6,23 +6,26 @@ package net.bryansaunders.feed_cleaner;
 import java.io.IOException;
 import java.net.SocketException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPClientConfig;
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPListParseEngine;
-import org.apache.commons.net.ftp.FTPReply;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.enterprisedt.net.ftp.FTPClient;
+import com.enterprisedt.net.ftp.FTPException;
+import com.enterprisedt.net.ftp.FTPFile;
 
 /**
  * Feed Cleaner Main Class. Configures and Starts Application.
@@ -32,215 +35,249 @@ import org.apache.logging.log4j.Logger;
  */
 public class FeedCleaner {
 
-    private static final Logger LOGGER = LogManager.getLogger(FeedCleaner.class);
+	private static final Logger LOGGER = LogManager
+			.getLogger(FeedCleaner.class);
 
-    private static final String CONFIG_FILE = "feedCleaner.properties";
+	private static final String CONFIG_FILE = "feedCleaner.properties";
 
-    private static final String FTP_HOSTNAME = "ftp.host";
+	private static final String FTP_HOSTNAME = "ftp.host";
 
-    private static final String FTP_PORT = "ftp.port";
+	private static final String FTP_PORT = "ftp.port";
 
-    private static final String FTP_USERNAME = "ftp.username";
+	private static final String FTP_USERNAME = "ftp.username";
 
-    private static final String FTP_PASSWORD = "ftp.password";
+	private static final String FTP_PASSWORD = "ftp.password";
 
-    private static final String FEED_DIRECTORY = "feed.directory";
+	private static final String FEED_DIRECTORY = "feed.directory";
 
-    private static final String FEED_DAYS_OLD = "feed.days_old";
-    
-    private List<String> ignoreList;
+	private static final String FEED_DAYS_OLD = "feed.days_old";
 
-    private PropertiesConfiguration config;
+	private List<String> ignoreList;
 
-    /**
-     * Main Method.
-     * 
-     * @param cmdArgs
-     *            Command Line Arguments.
-     */
-    public static void main(String[] cmdArgs) {
-        LOGGER.info("Feed Cleaner Started.");
+	private PropertiesConfiguration config;
 
-        int exitCode = 0;
+	/**
+	 * Main Method.
+	 * 
+	 * @param cmdArgs
+	 *            Command Line Arguments.
+	 */
+	public static void main(String[] cmdArgs) {
+		LOGGER.info("Feed Cleaner Started.");
 
-        FeedCleaner fc = new FeedCleaner();
+		int exitCode = 0;
 
-        try {
-            fc.configure();
-            fc.cleanFeeds();
-        } catch (ConfigurationException ce) {
-            LOGGER.error("An Error Occured Configuring the Feed Cleaner, Exiting.", ce);
-            exitCode = 1;
-        } catch (IOException ioe) {
-            LOGGER.error("An Error Occured Communicating with the Server, Exiting.", ioe);
-            exitCode = 2;
-        }
+		FeedCleaner fc = new FeedCleaner();
 
-        LOGGER.info("Feed Cleaner Exiting...");
-        System.exit(exitCode);
-    }
+		try {
+			fc.configure();
+			fc.cleanFeeds();
+		} catch (ConfigurationException ce) {
+			LOGGER.error(
+					"An Error Occured Configuring the Feed Cleaner, Exiting.",
+					ce);
+			exitCode = 1;
+		} catch (IOException ioe) {
+			LOGGER.error(
+					"An IO Error Occured Communicating with the Server, Exiting.",
+					ioe);
+			exitCode = 2;
+		} catch (FTPException e) {
+			LOGGER.error(
+					"A FTP Error Occured Communicting with the Server, Exiting.",
+					e);
+			exitCode = 3;
+		} catch (ParseException e) {
+			LOGGER.error(
+					"A Parse Error Occured Communicting with the Server, Exiting.",
+					e);
+			exitCode = 4;
+		}
 
-    /**
-     * Connects to the FTP Servers and Removes Stale Feeds.
-     * 
-     * @throws IOException
-     * @throws SocketException
-     */
-    private void cleanFeeds() throws SocketException, IOException {
-        FTPClient ftpClient = this.createFtpClient();
-        
-        this.connectToServer(ftpClient);
-        this.openFeedDirectory(ftpClient);
-        this.listAndRemoveFeeds(ftpClient);
-        this.disconnectServer(ftpClient);
-    }
+		LOGGER.info("Feed Cleaner Exiting...");
+		System.exit(exitCode);
+	}
 
-    private void openFeedDirectory(FTPClient ftpClient) throws IOException {
-        LOGGER.info("Opening Feed Directory...");
-        String directory = this.config.getString(FEED_DIRECTORY);
-        boolean result = ftpClient.changeWorkingDirectory(directory);
-        if (!result) {
-            this.disconnectServer(ftpClient);
-            throw new SocketException("Server Error Opening Feed Directory.");
-        }
-        LOGGER.info("Changed to Feed Directory.");
-    }
+	/**
+	 * Connects to the FTP Servers and Removes Stale Feeds.
+	 * 
+	 * @throws IOException
+	 * @throws SocketException
+	 * @throws FTPException
+	 * @throws ParseException
+	 */
+	private void cleanFeeds() throws SocketException, IOException,
+			FTPException, ParseException {
+		FTPClient ftpClient = this.createFtpClient();
 
-    private void listAndRemoveFeeds(FTPClient ftpClient) throws IOException {
-        LOGGER.info("Getting File List...");
-        if(LOGGER.isDebugEnabled()){
-        	LOGGER.debug("Current Directory: " + ftpClient.printWorkingDirectory());
-        }
-        ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
-        FTPListParseEngine engine = ftpClient.initiateListParsing("net.bryansaunders.feed_cleaner.Mp4FileParser", ".");
+		this.connectToServer(ftpClient);
+		this.listAndRemoveFeeds(ftpClient);
+		this.disconnectServer(ftpClient);
+	}
 
-        LOGGER.info("Checking Files...");
-        // Loop over File List
-        while (engine.hasNext()) {
-            // Get Files 25 at a time
-            FTPFile[] files = engine.getNext(25);
-            for (FTPFile file : files) {
-                
-                // Check for File Type
-                if(file.getType() == FTPFile.FILE_TYPE){
-                    String fileName = file.getName();
+	private void listAndRemoveFeeds(FTPClient ftpClient) throws IOException,
+			FTPException, ParseException {
+		LOGGER.info("Getting File List...");
+		String directory = this.config.getString(FEED_DIRECTORY);
+		FTPFile[] files = ftpClient.dirDetails(directory);
 
-                    // Print File Info
-                    if (LOGGER.isDebugEnabled()) {
-                        Date timestampDate = file.getTimestamp().getTime();
-                        String timestamp = DateFormat.getDateTimeInstance().format(timestampDate);
-                        LOGGER.debug("File: " + fileName + " : " + timestamp +" - " +file.getTimestamp().get(Calendar.YEAR));
-                    }
-                    
-                    if(this.ignoreList.contains(fileName)){
-                        LOGGER.info("Skipped File (Ignored): " + fileName);
-                        continue;
-                    }
+		LOGGER.info("Checking Files...");
+		for (FTPFile file : files) {
 
-                    // Check File Age
-                    if (this.fileIsStale(file)) {
-                        // Delete File
-                        //ftpClient.deleteFile(fileName);
-                        LOGGER.info("Deleted File: " + fileName);
-                    }else{
-                        LOGGER.info("Skipped File (Not Stale): " + fileName);
-                    }
-                }
-            }
-        }
-        LOGGER.info("File Check Complete.");
-    }
+			// Check for File Type
+			if (!file.isDir()) {
+				String fileName = file.getName();
 
-    private boolean fileIsStale(FTPFile file) {
-        boolean isStale = false;
-        Integer daysOld = this.config.getInt(FEED_DAYS_OLD);
-        Date fileDate = file.getTimestamp().getTime();
+				// Print File Info
+				if (LOGGER.isDebugEnabled()) {
+					Date timestampDate = this.getDateFromFileName(fileName);
+					String timestamp = DateFormat.getDateTimeInstance().format(
+							timestampDate);
+					LOGGER.debug("File: " + fileName + " : " + timestamp);
+				}
 
-        long diffInMillies = System.currentTimeMillis() - fileDate.getTime();
-        long age = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+				if (this.ignoreList.contains(fileName)) {
+					LOGGER.info("Skipped File (Ignored): " + fileName);
+					continue;
+				}
 
-        if (age > daysOld) {
-            isStale = true;
-        }
+				// Check File Age
+				if (this.fileIsStale(file)) {
+					// Delete File
+					// ftpClient.deleteFile(fileName);
+					LOGGER.info("Deleted File: " + fileName);
+				} else {
+					LOGGER.info("Skipped File (Not Stale): " + fileName);
+				}
+			}
+		}
+		LOGGER.info("File Check Complete.");
+	}
 
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug(file.getName() + " is " + age + " Days Old, Stale: " + isStale);
-        }
+	private boolean fileIsStale(FTPFile file) {
+		boolean isStale = false;
+		Integer daysOld = this.config.getInt(FEED_DAYS_OLD);
+		Date fileDate = this.getDateFromFileName(file.getName());
 
-        return isStale;
-    }
+		long diffInMillies = System.currentTimeMillis() - fileDate.getTime();
+		long age = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 
-    private void disconnectServer(FTPClient ftpClient) throws IOException {
-        LOGGER.info("Disconnecting from Server...");
-        ftpClient.logout();
-        ftpClient.disconnect();
-        LOGGER.info("Server Disconnected.");
-    }
+		if (age > daysOld) {
+			isStale = true;
+		}
 
-    private void connectToServer(FTPClient ftpClient) throws SocketException, IOException {
-        LOGGER.info("Opening Connection to Server...");
-        String hostname = this.config.getString(FTP_HOSTNAME);
-        Integer port = this.config.getInt(FTP_PORT);
-        ftpClient.connect(hostname, port);
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug(file.getName() + " is " + age + " Days Old, Stale: "
+					+ isStale);
+		}
 
-        int reply = ftpClient.getReplyCode();
-        if (!FTPReply.isPositiveCompletion(reply)) {
-            ftpClient.disconnect();
-            throw new SocketException("Server Refused Connection.");
-        }
-        LOGGER.info("Connected to Server.");
+		return isStale;
+	}
 
-        LOGGER.info("Passing Login Information...");
-        String username = this.config.getString(FTP_USERNAME);
-        String password = this.config.getString(FTP_PASSWORD);
-        ftpClient.login(username, password);
+	private Date getDateFromFileName(String name) {
+		Date date = new Date();
+		try {
+			Pattern nameRegex = Pattern
+					.compile(
+							"([a-z-_]*)\\.([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})\\.mp4",
+							Pattern.CANON_EQ);
+			Matcher nameRegexMatcher = nameRegex.matcher(name);
+			boolean nameMatched = nameRegexMatcher.find();
+			if (nameMatched) {
+				Calendar cal = Calendar.getInstance();
+				cal.set(Calendar.YEAR,
+						Integer.valueOf(nameRegexMatcher.group(2)));
+				cal.set(Calendar.MONTH,
+						Integer.valueOf(nameRegexMatcher.group(3)) - 1);
+				cal.set(Calendar.DAY_OF_MONTH,
+						Integer.valueOf(nameRegexMatcher.group(4)));
+				cal.set(Calendar.HOUR_OF_DAY,
+						Integer.valueOf(nameRegexMatcher.group(5)));
+				cal.set(Calendar.MINUTE,
+						Integer.valueOf(nameRegexMatcher.group(6)));
+				cal.set(Calendar.SECOND,
+						Integer.valueOf(nameRegexMatcher.group(7)));
+				date = cal.getTime();
+			}
+		} catch (PatternSyntaxException e) {
+			// intentionally do nothing
+		}
+		return date;
+	}
 
-        LOGGER.info("Logged in to Server.");
-    }
+	private void disconnectServer(FTPClient ftpClient) throws FTPException,
+			IOException {
+		LOGGER.info("Disconnecting from Server...");
+		ftpClient.quit();
+		LOGGER.info("Server Disconnected.");
+	}
 
-    /**
-     * Creates and Configures FTP Client.
-     * 
-     * @return @Link {@link FTPClient}
-     */
-    private FTPClient createFtpClient() {
-        LOGGER.info("Creating FTP Client...");
-        FTPClient client = new FTPClient();
+	private void connectToServer(FTPClient ftpClient) throws SocketException,
+			IOException, FTPException {
+		LOGGER.info("Opening Connection to Server...");
+		String hostname = this.config.getString(FTP_HOSTNAME);
+		Integer port = this.config.getInt(FTP_PORT);
+		ftpClient.setRemoteHost(hostname);
+		ftpClient.setRemotePort(port);
+		ftpClient.connect();
 
-        LOGGER.info("Configuring FTP Client...");
-        FTPClientConfig ftpConfig = new FTPClientConfig();
-        client.configure(ftpConfig);
+		/*
+		 * String reply = ftpClient.getLastValidReply().getReplyCode(); if
+		 * (!reply.equals("200")) { ftpClient.quit(); throw new
+		 * SocketException("Server Refused Connection, Code: " + reply); }
+		 */
+		LOGGER.info("Connected to Server.");
 
-        LOGGER.info("FTP Client Ready.");
-        return client;
-    }
+		LOGGER.info("Passing Login Information...");
+		String username = this.config.getString(FTP_USERNAME);
+		String password = this.config.getString(FTP_PASSWORD);
+		ftpClient.login(username, password);
 
-    /**
-     * Configures the Feed Cleaner.
-     * 
-     * @throws ConfigurationException
-     */
-    private void configure() throws ConfigurationException {
-        LOGGER.info("Configuring Feed Cleaner...");
+		LOGGER.info("Logged in to Server.");
+	}
 
-        this.config = new PropertiesConfiguration(CONFIG_FILE);
-        this.config.setAutoSave(true);
-        this.config.setReloadingStrategy(new FileChangedReloadingStrategy());
+	/**
+	 * Creates and Configures FTP Client.
+	 * 
+	 * @return @Link {@link FileTransferClient}
+	 */
+	private FTPClient createFtpClient() {
+		LOGGER.info("Creating FTP Client...");
+		FTPClient client = new FTPClient();
 
-        if (LOGGER.isDebugEnabled()) {
-            Iterator<?> keys = this.config.getKeys();
-            LOGGER.debug("Configuration Properties:");
-            while (keys.hasNext()) {
-                String key = (String) keys.next();
-                String value = (String) this.config.getProperty(key);
-                LOGGER.debug("Prop: " + key + " = " + value);
-            }
-        }
-        
-        this.ignoreList = new LinkedList<String>();
-        this.ignoreList.add(".htaccess");
+		LOGGER.info("Configuring FTP Client...");
+		// No Config
 
-        LOGGER.info("Feed Cleaner Configured.");
-    }
+		LOGGER.info("FTP Client Ready.");
+		return client;
+	}
+
+	/**
+	 * Configures the Feed Cleaner.
+	 * 
+	 * @throws ConfigurationException
+	 */
+	private void configure() throws ConfigurationException {
+		LOGGER.info("Configuring Feed Cleaner...");
+
+		this.config = new PropertiesConfiguration(CONFIG_FILE);
+		this.config.setAutoSave(true);
+		this.config.setReloadingStrategy(new FileChangedReloadingStrategy());
+
+		if (LOGGER.isDebugEnabled()) {
+			Iterator<?> keys = this.config.getKeys();
+			LOGGER.debug("Configuration Properties:");
+			while (keys.hasNext()) {
+				String key = (String) keys.next();
+				String value = (String) this.config.getProperty(key);
+				LOGGER.debug("Prop: " + key + " = " + value);
+			}
+		}
+
+		this.ignoreList = new LinkedList<String>();
+		this.ignoreList.add(".htaccess");
+
+		LOGGER.info("Feed Cleaner Configured.");
+	}
 
 }
